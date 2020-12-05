28d526acd4dd5371a5dcbe69349b7701a95ad7bf
space_mushroom <matteo.bresciani.997@gmail.com>
2020-12-05 22:09:39 +0000

    generalized parsing and indexing for all the documents

---
+++ search_engine_group_project/src/main/java/com/tcd/search_engine_group_project/DocumentIndexerMaps.java
@@ -0,0 +1,30 @@
+package com.tcd.search_engine_group_project;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DocumentIndexerMaps {
+    public static final Map<String, String> FT_MAP = new HashMap<String, String>() {{
+        put("id", "DOCNO");
+        put("title", "title");
+        put("text", "text");
+    }};
+
+    public static final Map<String, String> FR_MAP = new HashMap<String, String>() {{
+        put("id", "DOCNO");
+        put("title", "title");
+        put("text", "text");
+    }};
+
+    public static final Map<String, String> FBIS_MAP = new HashMap<String, String>() {{
+        put("id", "DOCNO");
+        put("title", "title");
+        put("text", "text");
+    }};
+
+    public static final Map<String, String> LA_TIMES_MAP = new HashMap<String, String>() {{
+        put("id", "DOCNO");
+        put("title", "title");
+        put("text", "text");
+    }};
+}
