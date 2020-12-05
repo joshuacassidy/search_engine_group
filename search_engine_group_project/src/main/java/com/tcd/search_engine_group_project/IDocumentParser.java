0b13cd1f4f06144df8a1c63fa9cc84e3ed9e47de
space_mushroom <matteo.bresciani.997@gmail.com>
2020-12-05 22:17:05 +0000

    removed previous implementations of parsers

--- search_engine_group_project/src/main/java/com/tcd/search_engine_group_project/IDocumentParser.java
+++
@@ -1,8 +0,0 @@
-package com.tcd.search_engine_group_project;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-public interface IDocumentParser {
-    void parseDocument(Path documentFolder) throws Exception;
-}
