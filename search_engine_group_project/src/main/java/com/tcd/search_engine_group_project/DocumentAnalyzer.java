b257b3a06780a197e3c4cd3081321ccd8a852efb
space_mushroom <matteo.bresciani.997@gmail.com>
2020-12-15 10:21:02 +0000

    modified analyzer

--- search_engine_group_project/src/main/java/com/tcd/search_engine_group_project/DocumentAnalyzer.java
+++ search_engine_group_project/src/main/java/com/tcd/search_engine_group_project/DocumentAnalyzer.java
@@ -1,15 +1,22 @@
 package com.tcd.search_engine_group_project;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.*;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
-import org.apache.lucene.analysis.en.EnglishPossessiveFilterFactory;
-import org.apache.lucene.analysis.ngram.NGramTokenizerFactory;
+
+import org.apache.lucene.analysis.miscellaneous.LengthFilter;
+import org.apache.lucene.analysis.miscellaneous.RemoveDuplicatesTokenFilter;
+import org.apache.lucene.analysis.miscellaneous.TrimFilter;
 import java.util.*;
-import org.apache.lucene.analysis.synonym.SynonymGraphFilterFactory;
-import org.apache.lucene.analysis.synonym.SynonymFilterFactory;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.lucene.analysis.shingle.ShingleAnalyzerWrapper;
+import org.apache.lucene.analysis.snowball.SnowballFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.tartarus.snowball.ext.EnglishStemmer;
 
 public class DocumentAnalyzer {
     public static Analyzer getCustomAnalyzer() throws IOException {
@@ -24,15 +31,38 @@ public class DocumentAnalyzer {
         sargs.put("synonyms", System.getProperty("user.dir") + "/prolog/wn_s.pl");
         sargs.put("format", "wordnet");
 
-        return CustomAnalyzer.builder(Paths.get(stopwordsFolder))
+        Set<String> stopWords;
+        try (Stream<String> lines = Files.lines(Paths.get(stopwordsFolder + stopwordsFile))) {
+            stopWords = new HashSet<>(lines.collect(Collectors.toList()));
+        }
+        CharArraySet stopWordsSet = CharArraySet.copy(stopWords);
+
+        return new StopwordAnalyzerBase() {
+            @Override
+            protected TokenStreamComponents createComponents(String field) {
+                Tokenizer tokenizer = new StandardTokenizer();
+
+                TokenStream filter = new TrimFilter(tokenizer);
+                filter = new LowerCaseFilter(filter);
+                filter = new StopFilter(filter, stopWordsSet);
+                filter = new SnowballFilter(filter, new EnglishStemmer());
+                filter = new LengthFilter(filter, 1, 15);
+                filter = new RemoveDuplicatesTokenFilter(filter);
+
+                return new TokenStreamComponents(tokenizer, filter);
+            }
+        };
+
+        /*return CustomAnalyzer.builder(Paths.get(stopwordsFolder))
+                .withTokenizer()
                 // .withTokenizer(
-                //     NGramTokenizerFactory.class, 
-                //     new String[] { 
-                //         "minGramSize", "1", 
-                //         "maxGramSize", "3" 
+                //     NGramTokenizerFactory.class,
+                //     new String[] {
+                //         "minGramSize", "1",
+                //         "maxGramSize", "3"
                 //     }
                 //     )
-                .withTokenizer("standard")
+                //.withTokenizer("standard")
                     .addTokenFilter(EnglishPossessiveFilterFactory.class)
                     .addTokenFilter("trim")
                     .addTokenFilter("lowercase")
@@ -42,9 +72,13 @@ public class DocumentAnalyzer {
                             "replace", "all",
                             "replacement", ""
                     )
-                    .addTokenFilter(SynonymFilterFactory.class, sargs)
-                    // .addTokenFilter(SynonymGraphFilterFactory.class, sargs)
-                    .addTokenFilter("snowballPorter") 
-                    .build();
+                    //.addTokenFilter(SynonymFilterFactory.class, sargs)
+                    //.addTokenFilter(SynonymGraphFilterFactory.class, sargs)
+                    .addTokenFilter("snowballPorter")
+                    .build();*/
+    }
+
+    public static Analyzer getCustomAnalyzerShingle() throws IOException {
+        return new ShingleAnalyzerWrapper(getCustomAnalyzer());
     }
 }
