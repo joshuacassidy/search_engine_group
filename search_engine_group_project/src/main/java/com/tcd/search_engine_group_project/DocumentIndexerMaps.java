4444ee7b0789017f531f241f599bb4989717dcc1
space_mushroom <matteo.bresciani.997@gmail.com>
2020-12-15 11:17:56 +0000

    small change for the indexing

--- search_engine_group_project/src/main/java/com/tcd/search_engine_group_project/DocumentIndexerMaps.java
+++ search_engine_group_project/src/main/java/com/tcd/search_engine_group_project/DocumentIndexerMaps.java
@@ -1,30 +1,33 @@
 package com.tcd.search_engine_group_project;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DocumentIndexerMaps {
-    public static final Map<String, String> FT_MAP = new HashMap<String, String>() {{
-        put("id", "DOCNO");
-        put("title", "headline");
-        put("text", "text");
+    public static final Map<String, List<String>> FT_MAP = new HashMap<String, List<String>>() {{
+        put("id", Arrays.asList("DOCNO"));
+        put("title", Arrays.asList("headline"));
+        put("text", Arrays.asList("text"));
     }};
 
-    public static final Map<String, String> FR_MAP = new HashMap<String, String>() {{
-        put("id", "DOCNO");
-        put("title", "title");
-        put("text", "text");
+    public static final Map<String, List<String>> FR_MAP = new HashMap<String, List<String>>() {{
+        put("id", Arrays.asList("DOCNO"));
+        put("title", Arrays.asList("title"));
+        put("text", Arrays.asList("text"));
     }};
 
-    public static final Map<String, String> FBIS_MAP = new HashMap<String, String>() {{
-        put("id", "DOCNO");
-        put("title", "ti");
-        put("text", "text");
+    public static final Map<String, List<String>> FBIS_MAP = new HashMap<String, List<String>>() {{
+        put("id", Arrays.asList("DOCNO"));
+        put("title", Arrays.asList("ti"));
+        put("text", Arrays.asList("text", "header"));
     }};
 
-    public static final Map<String, String> LA_TIMES_MAP = new HashMap<String, String>() {{
-        put("id", "DOCNO");
-        put("title", "headline");
-        put("text", "text");
+    public static final Map<String, List<String>> LA_TIMES_MAP = new HashMap<String, List<String>>() {{
+        put("id", Arrays.asList("DOCNO"));
+        put("title", Arrays.asList("headline"));
+        put("text", Arrays.asList("text", "graphic"));
     }};
 }
