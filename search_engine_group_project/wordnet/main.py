4db5df556f97e75b379fdd3fbb2fc3e0859fae35
joshuacassidy <joshcassidy79@gmail.com>
2020-12-19 23:05:51 +0000

    added add POS

--- search_engine_group_project/wordnet/main.py
+++ search_engine_group_project/wordnet/main.py
@@ -1,10 +1,20 @@
+import spacy
+
 syns = open("wordnet/words.txt","r") 
 file_content = syns.readlines()
 token_list = []
 count = 0
 
+model = spacy.load("en_core_web_md")
+
+words = {}
+
 for i in file_content:
 	token_list.append(i.split(" "))
+	if count == 0:
+		for word in model(i):
+			if word.tag_ == "NN" or word.tag_ == "NNS":
+				words["%s" % word] = "%s" % word
 	if count > 1000:
 		break
 	count += 1
@@ -19,25 +29,27 @@ model = Word2Vec(
     window=6,
     sample=1e-3)
 
-# model_name = "custom_model.wn"
-# model.save(model_name)
+model_name = "custom_model.wn"
+# model.wv.save_word2vec_format(model_name)
+
 
 syns.close()
 
-custom_syns = open("wordnet/custom_syns.txt","w+") 
+custom_syns = open("wordnet/custom_syns_py.txt","w+") 
 
 stopwords = []
 with open("resources/stop_words.txt", "r", encoding="utf-8") as f:
 	stopwords = f.read().split("\n")
 
+print(words)
 
 for i in model.wv.vocab.keys():
 	sims = str(i)
-	if i not in stopwords and not i.isnumeric() and i.strip().rstrip() != "":
+	if i not in stopwords and not i.isnumeric() and i.strip().rstrip() != "" and i in words:
 		similar_words = model.most_similar(positive=[i], topn=100, restrict_vocab=30000)
 		has_syns = False
 		for sim in similar_words:
-			if sim[0] not in stopwords and not sim[0].isnumeric():
+			if sim[0] not in stopwords and not sim[0].isnumeric() and sim[0] in words:
 				sims += ":" + str(sim[0]).strip().rstrip()
 				has_syns = True
 		if has_syns:
